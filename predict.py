4530226dfa5f2290fbb373f31fafd8bc6d433b22
조우성 <ender1412@naver.com>
2022-11-22 16:48:25 +0900

    역표준화코드추가

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@ from tensorflow import keras
 import firebase_admin
 from firebase_admin import credentials ,firestore
 import keyinfo as key
+import numpy as np
+import pipeline as pipe
 
 cred = credentials.Certificate("key/hackathon-11d72-firebase-adminsdk-7hnwq-45f6fa260b.json")
 firebase_admin.initialize_app(cred,{
@@ -14,6 +16,15 @@ for k in db.collection("test").get():
     input = k.to_dict()
 
 
-model = keras.models.load_model('/data/_1/mymodel')
+model = keras.models.load_model('/data/_'+str(pipe.checkpoint_count)+'/mymodel')
 result = model.predict(input)[0]
+
+result = result * np.std(pipe.y_valid[:,-1:],axis=0) + np.mean(pipe.y_valid[:,-1:],axis=0)
+result = result[:]*1000
+
+#test
+import matplotlib.pyplot as plt
+plt.plot(result)
+plt.show()
+
 
